9eb97864133afbfa9712213447e96cfa025c1d16
Bhupender Lawa <bhupenderlawa@Bhupenders-Laptop.local>
2024-01-26 12:12:34 +0530

    Unoptimized code h at this moment. After this commnit I will work on making it effiecient

--- degrees.py
+++ degrees.py
@@ -83,7 +83,7 @@ def main():
             movie = movies[path[i + 1][0]]["title"]
             print(f"{i + 1}: {person1} and {person2} starred in {movie}")
 
-
+### UNOPTIMIZED INEFFICIENT CODE VERSION-1.0
 def shortest_path(source, target):
     """
     Returns the shortest list of (movie_id, person_id) pairs
